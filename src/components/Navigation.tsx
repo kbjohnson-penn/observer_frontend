faf8b1745d65c6ac808fbac73ce5a71223647e50
BasamAlasaly <bhasaly@gmail.com>
2024-07-26 12:52:10 -0400

    fix: update logo width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,7 +24,7 @@ export default function Navigation() {
           <Link href="https://www.med.upenn.edu/">
             <Image
               src="/ObserverLogoDarkBackground.svg"
-              width={160}
+              width={220}
               height={80}
               alt="Penn Medicine logo"
               className="h-12 ml-4"
